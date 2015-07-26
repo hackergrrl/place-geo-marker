8e07692ac695ce5a98f545fcf26ee8ebcb83498e
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-07-25 23:38:32 -0700

    initial commit; works!

---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs')
+var http = require('http')
+var opn = require('opn')
+var shoe = require('shoe')
+
+module.exports = function (defaultLngLat, cb) {
+  defaultLngLat = defaultLngLat || { lat: -25.363882, lng: 131.044922 }
+  var pos = defaultLngLat
+  var port = 9691
+
+  // Serve the static files.
+  var server = http.createServer(function(req, res) {
+    if (/bundle.js/.test(req.url)) {
+      fs.createReadStream('bundle.js').pipe(res)
+    } else {
+      fs.createReadStream('map.html').pipe(res)
+    }
+  })
+
+  server.listen(port, function() {
+    opn('http://localhost:' + port + '?lon=' + defaultLngLat.lng + '&lat=' + defaultLngLat.lat)
+  })
+
+  var sock = shoe(function(stream) {
+    stream.on('end', function() {
+      cb(null, pos)
+    })
+    stream.on('close', function() {
+      cb(null, pos)
+    })
+    stream.on('data', function(data) {
+      pos = JSON.parse(data.toString())
+    })
+  })
+  sock.install(server, '/whereami')
+}
+
---
+++ map.js
@@ -0,0 +1,40 @@
+var getQueryParam = require('get-query-param')
+var shoe = require('shoe')
+
+function initialize() {
+  var defaultLon = getQueryParam('lon', window.location.href)
+  var defaultLat = getQueryParam('lat', window.location.href)
+  var defaultGeo
+  if (defaultLat && defaultLon) {
+    defaultGeo = { lon: defaultLon, lat: defaultLat }
+  } else {
+    // As good of a place as any!
+    defaultGeo = { lat: -25.363882, lng: 131.044922 }
+  }
+
+  var myLatlng = new google.maps.LatLng(defaultGeo.lat, defaultGeo.lon)
+  var mapOptions = {
+    zoom: 4,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'place-geo-marker',
+      draggable: true
+  });
+
+  var stream = shoe('/whereami')
+
+  google.maps.event.addListener(marker, 'dragend', function (event) {
+    var geo = {
+      lat: this.getPosition().lat(),
+      lng: this.getPosition().lng()
+    }
+    stream.write(JSON.stringify(geo))
+  })
+}
+
+google.maps.event.addDomListener(window, 'load', initialize)
