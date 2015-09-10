b4fa4973db7c38a0923276cc66f6729cdcad013e
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-09-10 10:26:35 -0700

    use getport for port selection

--- index.js
+++ index.js
@@ -3,42 +3,47 @@ var http = require('http')
 var opn = require('opn')
 var path = require('path')
 var shoe = require('shoe')
+var getport = require('getport')
 
 module.exports = function (defaultLngLat, cb) {
   defaultLngLat = defaultLngLat || { lat: -25.363882, lng: 131.044922 }
   var pos = defaultLngLat
-  var port = 9691
 
-  // Serve the static files.
-  var server = http.createServer(function(req, res) {
-    if (/bundle.js/.test(req.url)) {
-      fs.createReadStream(path.join(__dirname, 'bundle.js')).pipe(res)
-    } else {
-      fs.createReadStream(path.join(__dirname, 'map.html')).pipe(res)
-    }
+  getport(5000, 5999, function (err, port) {
+    if (err) throw err
+    else run(port)
   })
 
-  server.setTimeout(1000)
+  function run (port) {
+    // Serve the static files.
+    var server = http.createServer(function(req, res) {
+      if (/bundle.js/.test(req.url)) {
+        fs.createReadStream(path.join(__dirname, 'bundle.js')).pipe(res)
+      } else {
+        fs.createReadStream(path.join(__dirname, 'map.html')).pipe(res)
+      }
+    })
 
-  server.listen(port, function() {
-    opn('http://localhost:' + port
-        + '?lon=' + defaultLngLat.lng
-        + '&lat=' + defaultLngLat.lat)
-  })
+    server.setTimeout(1000)
 
-  var sock = shoe(function(stream) {
-    stream.on('end', function() {
-      server.close()
-      cb(null, pos)
+    server.listen(port, function() {
+      opn('http://localhost:' + port
+          + '?lon=' + defaultLngLat.lng
+          + '&lat=' + defaultLngLat.lat)
     })
-    stream.on('close', function() {
-      server.close()
-      cb(null, pos)
+
+    var sock = shoe(function(stream) {
+      function end() {
+        server.close()
+        cb(null, pos)
+      }
+      stream.on('end', end)
+      stream.on('close', end)
+      stream.on('data', function(data) {
+        pos = JSON.parse(data.toString())
+      })
     })
-    stream.on('data', function(data) {
-      pos = JSON.parse(data.toString())
-    })
-  })
-  sock.install(server, '/whereami')
+    sock.install(server, '/whereami')
+  }
 }
 
